5707b3bf4b1bf15822d3bcd4433457bd02a0bd76
Ryan Tablada <ryan.tablada@gmail.com>
2013-07-29 18:31:14 -0400

    Created Eloquent Model Filtered

---
+++ src/Rtablada/Profane/model.php
@@ -0,0 +1,32 @@
+<?php namespace Rtablada\Profane;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use App;
+
+abstract class Model extends Eloquent
+{
+	/**
+	 * Attributes to filter profanity of
+	 * @var array
+	 */
+	protected $filtered = array();
+
+	protected $filterReplace = '';
+
+	/**
+	 * Set a given attribute on the model.
+	 * Check if filtered first.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return void
+	 */
+	public function setAttribute($key, $value)
+	{
+		if (array_key_exists($key, $this->filtered)) {
+			$value = Filter::filter($value, $this->filterReplace);
+		}
+
+		parent::setAttribute($key, $value);
+	}
+}
